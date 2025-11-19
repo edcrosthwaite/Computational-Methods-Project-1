0cfb99f8b81d052cea1035712b26d7f722a6f4b5
Owen-Mckenzie-EDIUNI <s2521458@ed.ac.uk>
2025-11-19 22:14:52 +0000

    Main.py tweak

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.ODEnew import ode_bump
+from src.ODEnew import ode
 from src.ks_fine_tune import ks_rootfinding
 from src.passive_pareto import passive_suspension_pareto
 from src.skyhook_pareto import skyhook_suspension_pareto
@@ -6,8 +6,8 @@ from src.Peak_Acc_Interol import inter
 
 if __name__ == "__main__":
     inter()
-    ode_bump(test_type="bump")
-    ode_bump(test_type="iso")
+    ode(test_type="bump")
+    ode(test_type="iso")
     ks_rootfinding()
     passive_suspension_pareto()
     skyhook_suspension_pareto()
